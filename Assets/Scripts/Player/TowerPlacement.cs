78fa4c65cc0020ad53d13d2e58e088b2feca16e7
mekkmann <alex.p.liljekvist@gmail.com>
2025-04-17 23:43:45 +0200

    Refactor TowerPlacement
    - TowerPlacement
    -- Extracted Update logic to smaller methods

--- Assets/Scripts/Player/TowerPlacement.cs
+++ Assets/Scripts/Player/TowerPlacement.cs
@@ -6,12 +6,8 @@ public class TowerPlacement : MonoBehaviour
     [SerializeField] private LayerMask _placementCheckMask;
     [SerializeField] private LayerMask _placementCollideMask;
     [SerializeField] private GameObject _currentTowerToPlace;
+    private BoxCollider _currentTowerToPlaceCollider;
     [SerializeField] private GameObject _testTower;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-
-    }
 
     // Update is called once per frame
     void Update()
@@ -23,35 +19,56 @@ public class TowerPlacement : MonoBehaviour
 
         if (_currentTowerToPlace != null)
         {
-            BoxCollider towerToPlaceCollider = _currentTowerToPlace.GetComponent<BoxCollider>();
-            towerToPlaceCollider.isTrigger = true;
-            Ray camRay = _playerCamera.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(camRay, out RaycastHit hitInfo, 100f, _placementCollideMask, QueryTriggerInteraction.Ignore))
+            RaycastHit hitInfo = MoveUnplacedTowerToRaycastPosition();
+
+
+            if (CanPlaceTower(hitInfo))
             {
-                _currentTowerToPlace.transform.position = hitInfo.point;
-                Debug.Log($"Hit: {hitInfo.collider.gameObject}");
-            }
-            Vector3 boxCenter = _currentTowerToPlace.transform.position + towerToPlaceCollider.center;
-            Vector3 halfExtents = towerToPlaceCollider.size * 0.5f;
-            bool isCollidingWithOtherObject = Physics.CheckBox(boxCenter, halfExtents, Quaternion.identity, _placementCheckMask, QueryTriggerInteraction.Ignore);
-            if (hitInfo.collider.gameObject.CompareTag("CanPlace") && !isCollidingWithOtherObject)
-            {
+                // TODO: Refactor color change
                 _currentTowerToPlace.GetComponent<TowerBase>().SetColor(Color.white);
                 if (Input.GetMouseButtonDown(0))
                 {
-                    towerToPlaceCollider.isTrigger = false;
-                    _currentTowerToPlace = null;
+                    PlaceTower();
                 }
             }
             else
             {
+                // TODO: Refactor color change
                 _currentTowerToPlace.GetComponent<TowerBase>().SetColor(Color.red);
             }
         }
     }
 
+    private bool CanPlaceTower(RaycastHit hitInfo)
+    {
+        return hitInfo.collider.gameObject.CompareTag("CanPlace") && !IsCollidingWithAnotherObject();
+    }
+    private bool IsCollidingWithAnotherObject()
+    {
+        Vector3 boxCenter = _currentTowerToPlace.transform.position + _currentTowerToPlaceCollider.center;
+        Vector3 halfExtents = _currentTowerToPlaceCollider.size * 0.5f;
+        return Physics.CheckBox(boxCenter, halfExtents, Quaternion.identity, _placementCheckMask, QueryTriggerInteraction.Ignore);
+    }
+    private RaycastHit MoveUnplacedTowerToRaycastPosition()
+    {
+        Ray camRay = _playerCamera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(camRay, out RaycastHit hitInfo, 100f, _placementCollideMask, QueryTriggerInteraction.Ignore))
+        {
+            _currentTowerToPlace.transform.position = hitInfo.point;
+        }
+
+        return hitInfo;
+    }
+    private void PlaceTower()
+    {
+        _currentTowerToPlaceCollider.isTrigger = false;
+        _currentTowerToPlace = null;
+        _currentTowerToPlaceCollider = null;
+    }
     public void SetTowerToPlace(GameObject tower)
     {
         _currentTowerToPlace = Instantiate(tower, Vector3.zero, Quaternion.identity);
+        _currentTowerToPlaceCollider = _currentTowerToPlace.GetComponent<BoxCollider>();
+        _currentTowerToPlaceCollider.isTrigger = true;
     }
 }
